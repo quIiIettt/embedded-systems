24bb89fbef1fd18e2d5a084675ba78bd11daab75
dqie.ee <103836441+quIiIettt@users.noreply.github.com>
2024-04-13 13:45:51 +0300

    update

--- IoT/lab5/lab5.ino
+++ IoT/lab5/lab5.ino
@@ -3,25 +3,27 @@
 const char* ssid = "Redmi"; 
 const char* password = "1234567890"; 
 
-void setup()
-{
+void setup() {
   delay(1000);
   Serial.begin(115200);
  
-  WiFi.begin(ssid, password);
+  connectToWiFi();
+}
 
+void loop() {
+}
+
+void connectToWiFi() {
+  WiFi.begin(ssid, password);
   Serial.println();
-  Serial.print("Connecting");
-  while (WiFi.status() != 5)
-  {
+  Serial.print("Connecting to WiFi");
+  
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
-  Serial.println("success!");
-  Serial.print("IP Address is: ");
+  Serial.println("\nWiFi connected successfully!");
+  Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
-}
-
-void loop() {
 }
